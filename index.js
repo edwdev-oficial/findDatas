877c82ef10b84a57b1f5f8102126733fd21e723c
Eduardo Oliveira <edjdevel@gmail.com>
2024-06-03 17:13:03 -0300

    Include datas in objects

--- index.js
+++ index.js
@@ -4,31 +4,38 @@ const valoresCCs = {
 
         'TE - 3' : {
             codigo: '2122713',
-            'taxa mensasl': 140.56
+            'taxa mensasl': 140.56,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hc7/h5a/10041990512670.png'
         },
         'TE - 500': {
             codigo: '2146846',
-            'taxa mensasl': 239.20
+            'taxa mensasl': 239.20,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hbf/h8e/10042112081950.png'
         },
         'TE - 700': {
             codigo: '438579',
-            'taxa mensasl': 350.9       
+            'taxa mensasl': 350.9,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h02/h5c/10050349760542.png'
         },
         'TE - 800': {
             codigo: '2064703',
-            'taxa mensasl': 375.06   
+            'taxa mensasl': 375.06,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h17/h92/10029397835806.png'
         },
         'TE - 1000': {
             codigo: '2167744',
-            'taxa mensasl': 480.76    
+            'taxa mensasl': 480.76,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hce/h54/10042093273118.png'
         },
         'TE - 2000': {
             codigo: '2149475',
-            'taxa mensasl': 635.66   
+            'taxa mensasl': 635.66,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/ha4/hd0/10042168705054.png'
         },
         'TE - 3000': {
             codigo: '2229065',
-            'taxa mensasl': 820.60  
+            'taxa mensasl': 820.60,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h69/hea/10042142752798.png'
         }
 
     },
@@ -37,31 +44,38 @@ const valoresCCs = {
 
         'TE - 3' : {
             codigo: '2122713',
-            'Preço Net': 2362.05
+            'Preço Net': 2362.05,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hc7/h5a/10041990512670.png'
         },
         'TE - 500': {
             codigo: '2146846',
-            'Preço Net': 4175.13
+            'Preço Net': 4175.13,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hbf/h8e/10042112081950.png'
         },
         'TE - 700': {
             codigo: '438579',
-            'Preço Net': 5885.62 
+            'Preço Net': 5885.62,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h02/h5c/10050349760542.png'
         },
         'TE - 800': {
             codigo: '2064703',
-            'Preço Net': 6991.92 
+            'Preço Net': 6991.92,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h17/h92/10029397835806.png'
         },
         'TE - 1000': {
             codigo: '2167744',
-            'Preço Net': 7885.28   
+            'Preço Net': 7885.28,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/hce/h54/10042093273118.png'
         },
         'TE - 2000': {
             codigo: '2149475',
-            'Preço Net': 10210.05 
+            'Preço Net': 10210.05,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/ha4/hd0/10042168705054.png'
         },
         'TE - 3000': {
             codigo: '2229065',
-            'Preço Net': 20853.02
+            'Preço Net': 20853.02,
+            imgLink: 'https://www.hilti.com.br/medias/sys_master/images/h69/hea/10042142752798.png'
         }        
 
     }
