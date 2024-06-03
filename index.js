c707f30103adcfa1e8fef47b06719882dc7790eb
Eduardo Oliveira <edjdevel@gmail.com>
2024-06-03 16:17:03 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const valoresCCs = {
+
+    'Gestão de Frotas': {
+
+        'TE - 3' : {
+            codigo: '2122713',
+            'taxa mensasl': 140.56
+        },
+        'TE - 500': {
+            codigo: '2146846',
+            'taxa mensasl': 239.20
+        },
+        'TE - 700': {
+            codigo: '438579',
+            'taxa mensasl': 350.9       
+        },
+        'TE - 800': {
+            codigo: '2064703',
+            'taxa mensasl': 375.06   
+        },
+        'TE - 1000': {
+            codigo: '2167744',
+            'taxa mensasl': 480.76    
+        },
+        'TE - 2000': {
+            codigo: '2149475',
+            'taxa mensasl': 635.66   
+        },
+        'TE - 3000': {
+            codigo: '2229065',
+            'taxa mensasl': 820.60  
+        }
+
+    },
+
+    LTS: {
+
+        'TE - 3' : {
+            codigo: '2122713',
+            'Preço Net': 2362.05
+        },
+        'TE - 500': {
+            codigo: '2146846',
+            'Preço Net': 4175.13
+        },
+        'TE - 700': {
+            codigo: '438579',
+            'Preço Net': 5885.62 
+        },
+        'TE - 800': {
+            codigo: '2064703',
+            'Preço Net': 6991.92 
+        },
+        'TE - 1000': {
+            codigo: '2167744',
+            'Preço Net': 7885.28   
+        },
+        'TE - 2000': {
+            codigo: '2149475',
+            'Preço Net': 10210.05 
+        },
+        'TE - 3000': {
+            codigo: '2229065',
+            'Preço Net': 20853.02
+        }        
+
+    }
+
+};
+
+console.log ( valoresCCs );
